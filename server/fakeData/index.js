bd3d9fc56feaa701d154fbee0c6f7cafd202a96e
QuocToanTK95 <quoctoantk95@gmail.com>
2023-02-22 00:46:01 +0700

    setup server and router

---
+++ server/fakeData/index.js
@@ -0,0 +1,32 @@
+export default {
+  authors: [
+    {
+      id: 123,
+      name: "ToanTQ",
+    },
+    {
+      id: 999,
+      name: "ToanTQ11",
+    },
+  ],
+  folders: [
+    {
+      id: 1,
+      name: "Home",
+      createdAt: "2023-21-02",
+      authorId: 123,
+    },
+    {
+      id: 2,
+      name: "New Folder",
+      createdAt: "2023-20-02",
+      authorId: 999,
+    },
+    {
+      id: 3,
+      name: "Work",
+      createdAt: "2023-19-02",
+      authorId: 123,
+    },
+  ],
+};
