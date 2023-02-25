c6625336be778579406438759ab2c34aeeb8ee01
QuocToanTK95 <quoctoantk95@gmail.com>
2023-02-26 01:06:07 +0700

    Connect to DB with MongoDB

---
+++ server/resolvers/index.js
@@ -0,0 +1,39 @@
+import { FolderModel } from "../models/index.js";
+
+export const resolvers = {
+  Query: {
+    folders: async () => {
+      const folders = await FolderModel.find();
+      console.log({ folders });
+      return folders;
+    },
+    folder: async (parent, args) => {
+      const folderId = args.folderId;
+      console.log({ folderId });
+      const foundFolder = await FolderModel.findOne({
+        _id: folderId,
+      });
+      return foundFolder;
+    },
+    note: (parent, args) => {
+      const noteId = args.noteId;
+      return fakeData.notes.find((note) => note.id === noteId);
+    },
+  },
+  Folder: {
+    author: (parent, args) => {
+      const authorId = parent.authorId;
+      return fakeData.authors.find((author) => author.id === authorId);
+    },
+    notes: (parent, args) => {
+      return fakeData.notes.filter((note) => note.folderId === parent.id);
+    },
+  },
+  Mutation: {
+    addFolder: async (parent, args) => {
+      const newFolder = new FolderModel({ ...args, authorId: "123" });
+      await newFolder.save();
+      return newFolder;
+    },
+  },
+};
--- server/server.mjs
+++ server/server.mjs
@@ -5,60 +5,18 @@ import express from "express";
 import cors from "cors";
 import http from "http";
 import { expressMiddleware } from "@apollo/server/express4";
-import fakeData from "./fakeData/index.js";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+import { resolvers } from "./resolvers/index.js";
+import { typeDefs } from "./schemas/index.js";
 
 const app = express();
 const httpServer = http.createServer(app);
 
-const typeDefs = `#graphql
-  type Folder {
-    id: String,
-    name: String,
-    createdAt: String,
-    author: Author,
-    notes: [Note]
-  }
-
-  type Note {
-    id: String,
-    content: String,
-  }
-
-  type Author {
-    id: String,
-    name: String,
-  }
-
-  type Query {
-    folders: [Folder],
-    folder(folderId: String): Folder,
-    note(noteId: String): Note,
-  }
-`;
-const resolvers = {
-  Query: {
-    folders: () => {
-      return fakeData.folders;
-    },
-    folder: (parent, args) => {
-      const folderId = args.folderId;
-      return fakeData.folders.find((folder) => folder.id === folderId);
-    },
-    note: (parent, args) => {
-      const noteId = args.noteId;
-      return fakeData.notes.find((note) => note.id === noteId);
-    },
-  },
-  Folder: {
-    author: (parent, args) => {
-      const authorId = parent.authorId;
-      return fakeData.authors.find((author) => author.id === authorId);
-    },
-    notes: (parent, args) => {
-      return fakeData.notes.filter((note) => note.folderId === parent.id);
-    },
-  },
-};
+//Connect to DB
+const URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.cbaxrfs.mongodb.net/?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 4000;
 
 const server = new ApolloServer({
   typeDefs,
@@ -70,5 +28,14 @@ await server.start();
 
 app.use(cors(), bodyParser.json(), expressMiddleware(server));
 
-await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
-console.log("Server ready at http://localhost:4000");
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    console.log("Connected to DB");
+    await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
+    console.log("Server ready at http://localhost:4000");
+  });
